a422d472894ad5e10230118f9110b6d25d72ac73
Shail-Narsaria <shailnarsaria10@gmail.com>
2024-03-02 21:51:45 +0530

    few changes

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,24 +1,59 @@
 import "./Contact.css"
+import {motion} from "framer-motion"
+
+const variants={
+  initial:{
+    x:-300,
+    y:0,
+    opacity:0
+  },
+  animate:{
+    x:0,
+    y:0,
+    opacity:1,
+    transition:{
+      duration:1,
+      StaggerChildren:0.1
+    }
+  }
+}
+
+const formvariants={
+  initial:{
+    x:-300,
+    y:0,
+    opacity:0
+  },
+  animate:{
+    x:0,
+    y:0,
+    opacity:1,
+    transition:{
+      duration:0.2,
+      StaggerChildren:0.1
+    }
+  }
+}
 
 const Contact = () => {
   return (
-    <div className="contact">
-      <div className="contactTextCont">
-        <h1>LET'S CONNECT</h1>
+    <motion.div className="contact" variants={variants} initial="initial" whileInView="animate">
+      <motion.div className="contactTextCont" variants={variants}>
+        <h1 variants={variants}>LET'S CONNECT</h1>
         <div className="contactInfo">
             <h2>E-Mail</h2>
             <span>shailnarsaria18@gmail.com</span>
         </div>
-      </div>
-      <div className="formCont">
+      </motion.div>
+      <motion.div className="formCont" variants={formvariants}>
         <form>
             <input type="text" placeholder="Name" required></input>
             <input type="email" placeholder="E-Mail" required></input>
             <textarea placeholder="Message" rows={5}></textarea>
             <button>Submit</button>
         </form>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   )
 }
 
