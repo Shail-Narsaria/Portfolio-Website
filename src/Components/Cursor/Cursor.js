14b0fac0caa7deab276dd75bc4d89503c0990416
Shail-Narsaria <shailnarsaria10@gmail.com>
2024-03-03 17:15:40 +0530

    few changes

--- src/Components/Cursor/Cursor.js
+++ src/Components/Cursor/Cursor.js
@@ -1,24 +1,25 @@
-import "./Cursor.css"
-import {neonCursor} from "https://unpkg.com/threejs-toys@0.0.2/build/threejs-toys.module.cdn.min.js";
-
+import React, { useEffect } from 'react';
+import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.2/build/threejs-toys.module.cdn.min.js';
+import './Cursor.css';
 
 const Cursor = () => {
-
-    const root=document.getElementById("cursor");
-    const op={
-        el:root,
-        shaderPoints:16,
-        curvePoints: 80,
-        sleepRadiusX:10,
-        sleepRadiusY:10,
+  useEffect(() => {
+    const root = document.getElementById('Cursor');
+    const op = {
+      el: root,
+      shaderPoints: 16,
+      curvePoints: 80,
+      sleepRadiusX: 10,
+      sleepRadiusY: 10,
     };
     neonCursor(op);
+  }, []);
 
   return (
-    <div className="cursor">
-      
+    <div id="cursor">
+      {/* Additional content if needed */}
     </div>
-  )
-}
+  );
+};
 
-export default Cursor
+export default Cursor;
