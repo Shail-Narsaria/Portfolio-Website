fbbb78df5430da09d62aaccd1c8c8bb404c53d51
Shail-Narsaria <shailnarsaria10@gmail.com>
2024-03-07 21:28:19 +0530

    few changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HeroSection from './Components/HeroSection/HeroSection';
 import Navbar from './Components/Navbar/Navbar';
 import Contact from './Components/Contact/Contact';
 import Cursor from './Components/Cursor/Cursor';
+import Projects from './Components/Projects/Projects';
 
 function App() {
   return (
@@ -12,11 +13,13 @@ function App() {
         <Navbar/>
         <HeroSection/>
       </section>
-      <section id="About">About</section>
-      {/* <section id="Skills">Skills</section>
-      <section>Parralax</section>
-      <section id="Projects">Projects</section>
+      {/* <section id="About">About</section>
+      <section id="Skills">Skills</section>
       <section>Parralax</section> */}
+      <section id="Projects">
+        <Projects/>
+      </section>
+      {/* <section>Parralax</section> */}
       <section id="Contact">
         <Contact/>
       </section>
